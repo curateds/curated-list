2cd3896295bf752ce4a60ff87f4d3ef9a2533c39
Junjun Deng <cnjamesdeng@gmail.com>
2023-07-31 00:15:03 +0800

    Add import map to deno configuration file

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as base64 from "https://deno.land/std@0.192.0/encoding/base64.ts"
+export * as base64 from "std/encoding/base64.ts"
 
-export * as dotenv from "https://deno.land/std/dotenv/mod.ts"
+export * as dotenv from "std/dotenv/mod.ts"
 
-export * as yaml from "https://deno.land/std/yaml/mod.ts"
+export * as yaml from "std/yaml/mod.ts"
